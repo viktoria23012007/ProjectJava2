0a11bcdf15e452aa7846466aeab244f76a1dbd01
Виктория Вареник <viktoria.varenik@yandex.ru>
2022-05-27 21:01:48 +0300

    Создано приложение, рассчитывающее количество начисленных мили за купленный билет

---
+++ Main/Java2.java
@@ -0,0 +1,18 @@
+public class Java2 {
+
+    public static void main(String[] args) {
+
+    int ticketPrice  = 9090;
+
+    int mileRatio = 20;
+
+    int miles = ticketPrice / mileRatio;
+
+        System.out.println("Спасибо за покупку! Вам начислится "+miles +" бонусные(-х) мили(-ь).");
+
+
+}
+
+}
+
+}
---
+++ demo/src/main/java/com/example/demo/Main2.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int ticketPrice  = 9090;
+
+        int mileRatio = 20;
+
+        int miles = ticketPrice / mileRatio;
+
+        System.out.println("Спасибо за покупку! Вам начислится "+miles +" бонусные(-х) мили(-ь).");
+
+    }
+}
